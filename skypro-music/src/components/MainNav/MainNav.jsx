72334f69a241936b17738cfeee226f52bce74621
Alex-Viet <alex.night.power@gmail.com>
2023-08-16 17:47:42 +0300

    Манипуляции с меню при клике на бургер

--- skypro-music/src/components/MainNav/MainNav.jsx
+++ skypro-music/src/components/MainNav/MainNav.jsx
@@ -1,35 +1,30 @@
 import './MainNav.css';
+import { useState } from 'react';
+import NavBurger from './MainNavBurger/NavBurger';
+import NavLogo from './MainNavLogo/NavLogo';
+import NavMenu from './MainNavMenu/NavMenu';
 
 export default function MainNav() {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      toggleVisibility();
+    }
+  };
+
   return (
     <nav className="main__nav nav">
-      <div className="nav__logo logo">
-        <img className="logo__image" src="img/logo.png" alt="logo" />
-      </div>
-      <div className="nav__burger burger">
-        <span className="burger__line" />
-        <span className="burger__line" />
-        <span className="burger__line" />
-      </div>
-      <div className="nav__menu menu">
-        <ul className="menu__list">
-          <li className="menu__item">
-            <a href="!#" className="menu__link">
-              Главное
-            </a>
-          </li>
-          <li className="menu__item">
-            <a href="!#" className="menu__link">
-              Мой плейлист
-            </a>
-          </li>
-          <li className="menu__item">
-            <a href="../signin.html" className="menu__link">
-              Войти
-            </a>
-          </li>
-        </ul>
-      </div>
+      <NavLogo />
+      <NavBurger
+        toggleVisibility={toggleVisibility}
+        handleKeyPress={handleKeyPress}
+      />
+      {isVisible && <NavMenu />}
     </nav>
   );
 }
---
+++ skypro-music/src/components/MainNav/MainNavLogo/NavLogo.jsx
@@ -0,0 +1,9 @@
+import '../MainNav.css';
+
+export default function NavLogo() {
+  return (
+    <div className="nav__logo logo">
+      <img className="logo__image" src="img/logo.png" alt="logo" />
+    </div>
+  );
+}
