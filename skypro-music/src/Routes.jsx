b1aeb3b2f794108ee8eebcdef093d3b0353a536a
Alex-Viet <alex.night.power@gmail.com>
2023-09-28 07:39:34 +0300

    Верстка и стили страницы авторизации

--- skypro-music/src/Routes.jsx
+++ skypro-music/src/Routes.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { Login } from './pages/login/Login';
+import { AuthPage } from './pages/auth-page/AuthPage';
 import { Register } from './pages/register/Register';
 import { Category } from './pages/category/Category';
 import { Favorites } from './pages/favorites/Favorites';
@@ -10,7 +10,7 @@ import { ProtectedRoute } from './components/ProtectedRoute/ProtectedRoute';
 export function AppRoutes({ user, tracks, isLoading, setCurrentTrack, trackListError }) {
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
+      <Route path="/login" element={<AuthPage />} />
       <Route path="/register" element={<Register />} />
       <Route path="*" element={<NotFound />} />
 
